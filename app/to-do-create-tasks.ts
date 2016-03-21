76cf85ab6a86694035aebc37b783839799782f43
Jordan Meier <jbirdmeier@gmail.com>
2016-03-21 14:11:12 -0700

    add logic to display different forms depending on what type of task you choose

--- app/to-do-create-tasks.ts
+++ app/to-do-create-tasks.ts
@@ -2,30 +2,75 @@
 /// <reference path="to-do-people.ts" />
 /// <reference path="to-do-listing-functions.ts" />
 
-var people = ToDoList.people;
+$(document).ready(function () {
+  var taskType;
+  $('#workTaskForm').hide();
+  $('#homeTaskForm').hide();
+  $('#hobbyTaskForm').hide();
+  $('#type').change(function (){
+    taskType = $('#type').val();
+    if (taskType === "workTask") {
+      $('#workTaskForm').show();
+    } else if (taskType === "homeTask") {
+      $('#homeTaskForm').show();
+    } else {
+      $('#hobbyTaskForm').show();
+    }
+  });
 
-var tasks = [];
-tasks.push(new ToDoList.HomeTask("Do the dishes.", "High"));
-tasks.push(new ToDoList.HomeTask("Buy chocolate.", "Low", people.jordan));
-tasks.push(new ToDoList.HomeTask("Wash the laundry.", "High"));
 
-tasks.push(new ToDoList.HobbyTask("Practice origami."));
-tasks.push(new ToDoList.HobbyTask("Bake a pie."));
 
-var today = new Date();
-var tomorrow = new Date();
-tomorrow.setDate(today.getDate() + 1);
-var nextDay = new Date();
-nextDay.setDate(today.getDate() + 2);
+  $('#addTask').click(function() {
+    var description = $('#description').val();
+    var priority = $('#priority').val();
+    var homeTasks = [];
+    var workTasks = [];
+    var hobbyTasks = [];
+    if (taskType === "homeTask") {
+      homeTasks.push(new ToDoList.HomeTask(description, priority));
+    } else if (taskType === "workTask") {
+      // workTasks.push(new ToDoList.WorkTask(tomorrow, description, priority, people.thor));
+    } else {
 
-tasks.push(new ToDoList.WorkTask(tomorrow, "Go to meeting.", "Medium", people.thor));
-tasks.push(new ToDoList.WorkTask(tomorrow, "Save the world.", "High", people.thor));
-tasks.push(new ToDoList.WorkTask(tomorrow, "Buy a new shirt.", "Low", people.thor));
-tasks.push(new ToDoList.WorkTask(nextDay, "Clean ceiling.", "Low", people.loki));
+    }
+  });
+});
 
-console.log(tasks);
-var thorTasks = ToDoList.describeTasksForPerson(people.thor, tasks);
-console.log("Here are Thor's tasks: ");
-for(var task of thorTasks) {
-  console.log(task); 
-}
+
+
+
+
+
+
+
+// var people = ToDoList.people;
+//
+// var tasks = [];
+// tasks.push(new ToDoList.HomeTask("Do the dishes.", "High"));
+// tasks.push(new ToDoList.HomeTask("Buy chocolate.", "Low", people.jordan));
+// tasks.push(new ToDoList.HomeTask("Wash the laundry.", "High"));
+//
+// tasks.push(new ToDoList.HobbyTask("Practice origami."));
+// tasks.push(new ToDoList.HobbyTask("Bake a pie."));
+//
+// var today = new Date();
+// var tomorrow = new Date();
+// tomorrow.setDate(today.getDate() + 1);
+// var nextDay = new Date();
+// nextDay.setDate(today.getDate() + 2);
+//
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Go to meeting.", "Medium", people.thor));
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Save the world.", "High", people.thor));
+// tasks.push(new ToDoList.WorkTask(tomorrow, "Buy a new shirt.", "Low", people.thor));
+// tasks.push(new ToDoList.WorkTask(nextDay, "Clean ceiling.", "Low", people.loki));
+//
+
+
+  // console.log(tasks);
+  //
+  //
+  // var thorTasks = ToDoList.describeTasksForPerson(people.thor, tasks);
+  // console.log("Here are Thor's tasks: ");
+  // for(var task of thorTasks) {
+  //   console.log(task);
+  // }
